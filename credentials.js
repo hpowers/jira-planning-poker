fdc02a80e3480cad88e575f93bda47159bfc4774
Hunter Powers <hunter@wiretheplanet.com>
2016-03-01 15:48:18 -0500

    remove credentials

--- credentials.js
+++
@@ -1,8 +0,0 @@
-var credentials = {}
-
-// put your Jira credentials
-// note - username is usually not email address
-credentials.username = 'jira username';
-credentials.password = 'jira password'
-
-module.exports = credentials;
